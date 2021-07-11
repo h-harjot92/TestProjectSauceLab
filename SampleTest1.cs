fcf8f0cf37de249987f28a812b2eedc6c8b4c917
h-harjot92 <60389772+h-harjot92@users.noreply.github.com>
2021-07-11 22:32:48 +0530

    Update SampleTest1.cs

--- SampleTest1.cs
+++ SampleTest1.cs
@@ -18,14 +18,14 @@ namespace TestProjectSauceLab
             Assert.AreEqual(emailInput.Text, "standard_user");
             var passwordInput = Drivers.FindElementByAccessibilityId("test-Password");
             passwordInput.SendKeys("secret_sauce");
-            //   Assert.AreEqual(passwordInput.Text, "secret_sauce");
+             Assert.AreEqual(passwordInput.Text, "secret_sauce");
             var loginBtn = Drivers.FindElementByAccessibilityId("test-LOGIN");
             loginBtn.Click();
-            var cart = Drivers.FindElementByName("test-Cart");
-            Assert.IsTrue(cart.Displayed);
+           // var cart = Drivers.FindElementByName("test-Cart");
+           // Assert.IsTrue(cart.Displayed);
         }
 
-        [Test]
+       // [Test]
         public void Test2()
         {
             var entryFiled = Drivers.FindElementByAccessibilityId("entryAutoId");
