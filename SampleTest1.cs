57ef18f947dd7794ec4d56cce27555f7967a5bdf
h-harjot92 <60389772+h-harjot92@users.noreply.github.com>
2021-07-12 10:56:51 +0530

    Update SampleTest1.cs

--- SampleTest1.cs
+++ SampleTest1.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +11,7 @@ namespace TestProjectSauceLab
     {
 
         [Test]
+        [Category("Sanity")]
         public void Test1()
         {
             var emailInput = Drivers.FindElementByAccessibilityId("test-Username");
@@ -18,23 +19,24 @@ namespace TestProjectSauceLab
             Assert.AreEqual(emailInput.Text, "standard_user");
             var passwordInput = Drivers.FindElementByAccessibilityId("test-Password");
             passwordInput.SendKeys("secret_sauce");
-            // Assert.AreEqual(passwordInput.Text, "secret_sauce");
+            //   Assert.AreEqual(passwordInput.Text, "secret_sauce");
             var loginBtn = Drivers.FindElementByAccessibilityId("test-LOGIN");
             loginBtn.Click();
-           // var cart = Drivers.FindElementByName("test-Cart");
-           // Assert.IsTrue(cart.Displayed);
+            var cart = Drivers.FindElementByName("test-Cart");
+            Assert.IsTrue(cart.Displayed);
         }
 
-       // [Test]
+        [Test]
+        [Category("Regression")]
         public void Test2()
         {
-            var entryFiled = Drivers.FindElementByAccessibilityId("entryAutoId");
+            var entryFiled = Drivers.FindElementByAccessibilityId("test-Username");
             entryFiled.SendKeys("1");
             Assert.AreEqual(entryFiled.Text, "1");
-            var btnElement = Drivers.FindElementByAccessibilityId("changeBtn");
-            btnElement.Click();
-            var label1 = Drivers.FindElementByAccessibilityId("CoffeeName");
-            Assert.AreEqual(label1.Text, "Pizza");
+            //var btnElement = Drivers.FindElementByAccessibilityId("changeBtn");
+            //btnElement.Click();
+            //var label1 = Drivers.FindElementByAccessibilityId("CoffeeName");
+            //Assert.AreEqual(label1.Text, "Pizza");
         }
     }
 }
